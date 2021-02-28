1ac7cced23d9746bbd63bc707d76c6b9134c67a4
RaptorXZ <ouatis@gmail.com>
2021-02-28 21:19:21 +0100

    added about section

--- scripts/display.js
+++ scripts/display.js
@@ -2,3 +2,7 @@ function showHC4() {
     document.querySelector(".bracket-hc4").classList.toggle("show-bracket");
     document.querySelector(".main-countdown").classList.toggle("hide-content");
 }
+
+function showAbout() {
+    document.querySelector(".about-hc").classList.toggle("hide-content");
+}
