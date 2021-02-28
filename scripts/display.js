10c22130b8f8de5ef20fc98397ff1abca44f7db7
RaptorXZ <ouatis@gmail.com>
2021-02-28 09:57:46 +0100

    added hc4 main event bracket

---
+++ scripts/display.js
@@ -0,0 +1,4 @@
+function showHC4() {
+    document.querySelector(".bracket-hc4").classList.toggle("show-bracket");
+    document.querySelector(".main-countdown").classList.toggle("hide-content");
+}
--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,18 +1,18 @@
 function navDropdown() {
-    document.getElementById("navDropArrow").classList.toggle("fa-caret-up");
-    document.getElementById("tournamentDropdown").classList.toggle("showDropdown");
+    document.querySelector("#navDropArrow").classList.toggle("fa-caret-up");
+    document.querySelector("#tournamentDropdown").classList.toggle("showDropdown");
 }
 
 window.onclick = function(event) {
     if (!event.target.matches('.navDropBtn')) {
-        var dropdown = document.getElementById("tournamentDropdown");
+        var dropdown = document.querySelector("#tournamentDropdown");
         if (dropdown.classList.contains('showDropdown')) {
             dropdown.classList.remove('showDropdown');
         }
     }
 
     if (!event.target.matches('.navDropBtn')) {
-        var dropArrow = document.getElementById("navDropArrow");
+        var dropArrow = document.querySelector("#navDropArrow");
         if (dropArrow.classList.contains('fa-caret-up')) {
             dropArrow.classList.remove('fa-caret-up');
         }
