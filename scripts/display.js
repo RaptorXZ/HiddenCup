d6353cda7f98993641ecee8c28d8fad35675256a
RaptorXZ <ouatis@gmail.com>
2021-03-05 03:07:32 +0100

    added qualifier brackets

--- scripts/display.js
+++ scripts/display.js
@@ -1,11 +1,15 @@
 function showHC(cup) {
-    document.querySelector(".bracket-hc").classList.add("show-bracket");
+    document.querySelector(".bracket-hc").classList.add("show-hc-bracket");
+    document.querySelector("#main-event-bracket").classList.add("show-bracket");
     document.querySelector(".main-countdown").classList.add("hide-content");
     document.querySelector("#bracket-mainevent-button").classList.add("bracket-nav-active");
     document.querySelector("#bracket-qualifiers-button").classList.remove("bracket-nav-active");
+    document.querySelector("#hidden-cup-main-title").classList.add("show-bracket");
+    document.querySelector("#qualifier-bracket").classList.remove("show-bracket");
+    document.querySelector("#hidden-cup-qualifier-title").classList.remove("show-bracket");
 
     if (cup == 'hc1') {
-        document.querySelector("#hidden-cup-title").innerHTML = "Hidden Cup 1 Main Event";
+        document.querySelector("#hidden-cup-main-title").innerHTML = "Hidden Cup 1 Main Event";
         document.querySelector("#hidden-cup-round").innerHTML = "Round of 12 - Best of 5";
         document.querySelector("#hidden-cup-sf").innerHTML = "Semifinals - Best of 5";
         setPlayerNames('hc1');
@@ -14,53 +18,42 @@ function showHC(cup) {
         setPlayerFlags('hc1');
     }
     else if (cup == 'hc2') {
-        document.querySelector("#hidden-cup-title").innerHTML = "Hidden Cup 2 Main Event";
+        document.querySelector("#hidden-cup-main-title").innerHTML = "Hidden Cup 2 Main Event";
+        document.querySelector("#hidden-cup-round").innerHTML = "Round of 16 - Best of 5";
         document.querySelector("#hidden-cup-sf").innerHTML = "Semifinals - Best of 5";
+        document.querySelector("#hidden-cup-qualifier-title").innerHTML = "Hidden Cup 2 Qualifiers";
+        document.querySelector("#qualifier-round1-title").innerHTML = "Round of 16 - Best of 5";
+        document.querySelector("#qualifier-round2-title").innerHTML = "Round of 8 - Best of 5";
+        document.querySelector("#qualifier-round3-title").innerHTML = "Final Qualifiers - Best of 7";
         setPlayerNames('hc2');
         setPlayerScores('hc2');
         setMatchDates('hc2');
         setPlayerFlags('hc2');
     }
     else if (cup == 'hc3') {
-        document.querySelector("#hidden-cup-title").innerHTML = "Hidden Cup 3 Main Event";
+        document.querySelector("#hidden-cup-main-title").innerHTML = "Hidden Cup 3 Main Event";
+        document.querySelector("#hidden-cup-round").innerHTML = "Round of 16 - Best of 5";
+        document.querySelector("#hidden-cup-sf").innerHTML = "Semifinals - Best of 7";
+        document.querySelector("#hidden-cup-qualifier-title").innerHTML = "Hidden Cup 3 Qualifiers";
+        document.querySelector("#qualifier-round1-title").innerHTML = "Round of 32 - Best of 5";
+        document.querySelector("#qualifier-round2-title").innerHTML = "Final Qualifiers - Round of 16 - Best of 7";
         setPlayerNames('hc3');
         setPlayerScores('hc3');
         setMatchDates('hc3');
         setPlayerFlags('hc3');
     }
     else if (cup == 'hc4') {
-        document.querySelector("#hidden-cup-title").innerHTML = "Hidden Cup 4 Main Event";
-        /* Dates */
-        document.querySelector("#main-ro16-match1-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match1-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-ro16-match2-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match2-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-ro16-match3-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match3-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-ro16-match4-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match4-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-ro16-match5-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match5-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-ro16-match6-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match6-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-ro16-match7-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match7-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-ro16-match8-time").setAttribute("datetime", "2021-03-18");
-        document.querySelector("#main-ro16-match8-time").innerHTML = "18 March 2021";
-        document.querySelector("#main-qf-match1-time").setAttribute("datetime", "2021-03-19");
-        document.querySelector("#main-qf-match1-time").innerHTML = "19 March 2021";
-        document.querySelector("#main-qf-match2-time").setAttribute("datetime", "2021-03-19");
-        document.querySelector("#main-qf-match2-time").innerHTML = "19 March 2021";
-        document.querySelector("#main-qf-match3-time").setAttribute("datetime", "2021-03-19");
-        document.querySelector("#main-qf-match3-time").innerHTML = "19 March 2021";
-        document.querySelector("#main-qf-match4-time").setAttribute("datetime", "2021-03-19");
-        document.querySelector("#main-qf-match4-time").innerHTML = "19 March 2021";
-        document.querySelector("#main-sf-match1-time").setAttribute("datetime", "2021-03-20");
-        document.querySelector("#main-sf-match1-time").innerHTML = "20 March 2021";
-        document.querySelector("#main-sf-match2-time").setAttribute("datetime", "2021-03-20");
-        document.querySelector("#main-sf-match2-time").innerHTML = "20 March 2021";
-        document.querySelector("#main-gf-match1-time").setAttribute("datetime", "2021-03-21");
-        document.querySelector("#main-gf-match1-time").innerHTML = "21 March 2021";
+        document.querySelector("#hidden-cup-main-title").innerHTML = "Hidden Cup 4 Main Event";
+        document.querySelector("#hidden-cup-round").innerHTML = "Round of 16 - Best of 5";
+        document.querySelector("#hidden-cup-sf").innerHTML = "Semifinals - Best of 7";
+        document.querySelector("#hidden-cup-qualifier-title").innerHTML = "Hidden Cup 4 Qualifiers";
+        document.querySelector("#qualifier-round1-title").innerHTML = "Round of 64 - Best of 5";
+        document.querySelector("#qualifier-round2-title").innerHTML = "Round of 32 - Best of 5";
+        document.querySelector("#qualifier-round3-title").innerHTML = "Final Qualifiers - Round of 16 - Best of 7";
+        setPlayerNames('hc4');
+        setPlayerScores('hc4');
+        setMatchDates('hc4');
+        setPlayerFlags('hc4');
     }
 }
 
@@ -73,11 +66,20 @@ function toggleBracket(display) {
     {
         document.querySelector("#bracket-mainevent-button").classList.add("bracket-nav-active");
         document.querySelector("#bracket-qualifiers-button").classList.remove("bracket-nav-active");
+        document.querySelector("#main-event-bracket").classList.add("show-bracket");
+        document.querySelector("#qualifier-bracket").classList.remove("show-bracket");
+        document.querySelector("#hidden-cup-main-title").classList.add("show-bracket");
+        document.querySelector("#hidden-cup-qualifier-title").classList.remove("show-bracket");
     }
     else if (display == "qualifiers")
     {
         document.querySelector("#bracket-mainevent-button").classList.remove("bracket-nav-active");
         document.querySelector("#bracket-qualifiers-button").classList.add("bracket-nav-active");
+        document.querySelector("#main-event-bracket").classList.remove("show-bracket");
+        document.querySelector("#qualifier-bracket").classList.add("show-bracket");
+        document.querySelector("#hidden-cup-main-title").classList.remove("show-bracket");
+        document.querySelector("#hidden-cup-qualifier-title").classList.add("show-bracket");
+        // createQualifierBrackets();
     }
 }
 
@@ -102,13 +104,18 @@ function setPlayerNames(cup) {
         players = ['Mr_Yo','TaToH','TheMax','JorDan_23','dogao','Villese','ACCM',
         'Liereyy','MbL','DauT','BacT','Tim','TheViper','Nicov','Vivi','Hera',
         'Mr_Yo','TheMax','dogao','Liereyy','MbL','Tim','TheViper','Vivi','TheMax',
-        'Tim','MbL','TheViper','TheMax','TheViper']
+        'Liereyy','MbL','TheViper','TheMax','TheViper']
     }
     else if (cup == 'hc3') {
         players = ['Vivi','Liereyy','TaToh','Nicov','F1Re','DauT','Hera','LaaaaaN',
         'TheViper','ACCM','Mr_Yo','Villese','MbL','BacT','dogao','TheMax','Liereyy',
         'TaToH','DauT','Hera','TheViper','Mr_Yo','MbL','dogao','TaToH','Hera',
         'TheViper','dogao','Hera','TheViper'];
+    }
+    else if (cup == 'hc4') {
+        players = ['TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD',
+        'TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD',
+        'TBD','TBD','TBD','TBD','TBD','TBD','TBD','TBD'];
     }
 
     const a = document.getElementsByClassName("bracket-name");
@@ -132,6 +139,10 @@ function setPlayerScores(cup) {
     else if (cup == 'hc3') {
         scores = ['0','3','3','1','1','3','3','1','3','0','3','2','3','0','3',
         '0','2','3','2','3','3','1','2','3','1','4','4','1','0','4'];
+    }
+    else if (cup == 'hc4') {
+        scores = ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'];
     }
 
     const a = document.getElementsByClassName("bracket-number");
@@ -176,6 +187,17 @@ function setMatchDates(cup) {
         '2020-03-21','2020-03-21','2020-03-21','2020-03-22','2020-03-22',
         '2020-03-22'];
     }
+    if (cup == 'hc4') {
+        dates = ['18 March 2021','18 March 2021','18 March 2021','18 March 2021',
+        '19 March 2021','19 March 2021','19 March 2021','19 March 2021',
+        '20 March 2021','20 March 2021','20 March 2021','20 March 2021',
+        '21 March 2021','21 March 2021','21 March 2021'];
+
+        datetimes = ['2021-03-18','2021-03-18','2021-03-18','2021-03-18',
+        '2021-03-19','2021-03-19','2021-03-19','2021-03-19','2021-03-20',
+        '2021-03-20','2021-03-20','2021-03-20','2021-03-21','2021-03-21',
+        '2021-03-21'];
+    }
 
     const a = document.getElementsByClassName("bracket-date");
     const date = Array.from(a);
@@ -204,7 +226,7 @@ function setPlayerFlags(cup) {
         'flag-icon-no','flag-icon-ar','flag-icon-cn','flag-icon-ca',
         'flag-icon-cn','flag-icon-fi','flag-icon-br','flag-icon-at',
         'flag-icon-no','flag-icon-cn','flag-icon-no','flag-icon-cn',
-        'flag-icon-fi','flag-icon-cn','flag-icon-no','flag-icon-no',
+        'flag-icon-fi','flag-icon-at','flag-icon-no','flag-icon-no',
         'flag-icon-fi','flag-icon-no'];
     }
     else if (cup == 'hc3') {
@@ -216,6 +238,15 @@ function setPlayerFlags(cup) {
         'flag-icon-no','flag-icon-cn','flag-icon-no','flag-icon-br',
         'flag-icon-es','flag-icon-ca','flag-icon-no','flag-icon-br',
         'flag-icon-ca','flag-icon-no'];
+    }
+    else if (cup == 'hc4') {
+        flags = ['flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm',
+        'flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm',
+        'flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm',
+        'flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm',
+        'flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm',
+        'flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm','flag-icon-qm',
+        'flag-icon-qm'];
     }
 
     const a = document.getElementsByClassName("bracket-flag");
@@ -258,3 +289,44 @@ function setPlayerFlags(cup) {
         flag[i].classList.add(flags[i]);
     }
 }
+
+// function createQualifierBrackets() {
+//     /* Create list item with class bracket-item */
+//     const bracket = document.createElement("li");
+//     bracket.classList.add("bracket-item");
+//     document.querySelector("#qualifier-round2-list").appendChild(bracket);
+//     /* Create div with class bracket-match */
+//     const match = document.createElement("div");
+//     match.classList.add("bracket-match");
+//     bracket.appendChild(match);
+//     /* Create table with class bracket-table */
+//     const table = document.createElement("table");
+//     table.classList.add("bracket-table");
+//     match.appendChild(table);
+//     /* Create caption with class bracket-caption */
+//     const caption = document.createElement("caption");
+//     caption.classList.add("bracket-caption");
+//     table.appendChild(caption);
+//     /* Create time with class bracket-date */
+//     const time = document.createElement("time");
+//     time.classList.add("bracket-date");
+//     time.id = "qualifier-ro32-match2-time";
+//     time.innerHTML = "18 March 2021";
+//     time.setAttribute("datetime", "2021-03-18");
+//     caption.appendChild(time);
+    
+
+//     const bracket2 = document.createElement("li");
+//     bracket2.classList.add("bracket-item");
+//     document.querySelector("#qualifier-round1-list").appendChild(bracket2);
+//     const match2 = document.createElement("div");
+//     match2.classList.add("bracket-match");
+//     bracket2.appendChild(match2);
+
+//     const bracket3 = document.createElement("li");
+//     bracket3.classList.add("bracket-item");
+//     document.querySelector("#qualifier-round1-list").appendChild(bracket3);
+//     const match3 = document.createElement("div");
+//     match3.classList.add("bracket-match");
+//     bracket3.appendChild(match3);
+// }
